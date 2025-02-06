c65e8ddfc18602d6a8747c91b55ba9547d7ef1ea
Artur Gomes <arturg91@outlook.com>
2025-02-06 13:54:39 -0300

    add script sql with all mod

--- sistema_clientes.sql
+++ sistema_clientes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06/02/2025 às 17:13
+-- Tempo de geração: 06/02/2025 às 17:53
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
